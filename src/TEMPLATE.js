2005b7dc30633a4d655ba1c20b5aeb15b34fc197
noahshayne <145724750+noahshayne@users.noreply.github.com>
2023-10-22 16:25:37 -0400

    Update TEMPLATE.js

--- src/TEMPLATE.js
+++ src/TEMPLATE.js
@@ -3,12 +3,8 @@
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
 import HomeIcon from '@mui/icons-material/Home';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import SnapchatIcon from '@mui/icons-material/Twitter';
-import PinterestIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 
 
 // Data object that contains all of the data for the template.
@@ -16,106 +12,42 @@ const data = {
     profile: {
         name: "Noah Shayne",
         contacts: [
-            { icon: PhoneIcon, label: "Phone", value: "(978) 123-4567" },
-            { icon: EmailIcon, label: "Email", value: "sara_friends@student.uml.edu" },
-            { icon: HomeIcon, label: "Address", value: "123 Main St, City, Country" }
+            { icon: EmailIcon, label: "Email", value: "noah.shayne@gmail.com" }
         ],
         skills: [
-            { category: "Programming Languages:", skills: "C/C++, Java, Assembly" },
-            { category: "Scripting Languages:", skills: "Bash Shell, HTML" },
-            { category: "Operating Systems:", skills: "Unix/Linux, Mac OSX, Windows XP & Vista" },
-            { category: "Documentation:", skills: "MS Word, Excel, PowerPoint & Visio" },
+            { category: "Programming Languages:", skills: "Python" },
+            { category: "Scripting Languages:", skills: "HTML" },
+            { category: "Documentation:", skills: "Google Spreadsheets" },
         ],
         socialMedia: [
-            { icon: FacebookIcon },
-            { icon: InstagramIcon },
-            { icon: LinkedInIcon },
-            { icon: TwitterIcon },
-            { icon: SnapchatIcon },
-            { icon: PinterestIcon },
+            { icon: GitHubIcon },
         ],
         education: [
             {
-                degree: "M.S. in Computer Science",
-                date: "Anticipated May 20xx",
-                school: "University of Massachusetts Lowell - Lowell, MA",
-                gpa: "Overall GPA: 3.33, GPA in Major: 3.50"
-            },
-            {
                 degree: "B.S. in Computer Science",
-                date: "Anticipated May 20xx",
+                date: "Anticipated May 2027",
                 school: "University of Massachusetts Lowell - Lowell, MA",
-                gpa: "Overall GPA: 3.33, GPA in Major: 3.50"
+                gpa: "GPA: NA"
             },
+         
         ],
         experience: [
             {
-                title: "Front End Developer / w3schools.com",
-                date: "Jan 2015 - Current",
-                description: "Lorem ipsum dolor sit amet...",
+                title: "Stop and Shop - Store Associate",
+                date: "Summer 2023",
+                description: "Worked as a store associate at a local Stop and Shop. Main job duty was to interact with and assist customers on a daily basis as a self-checkout assistant. Other tasks included cleaning, organizing and stocking shelves, and retrieving shopping carriages from the parking lot.",
             },
             {
-                title: "Computer Lab Monitor / University of Massachusetts Lowell",
-                date: "Jan 20xx - Present",
-                description: "Monitor lab activity for up to 50 users at a time. Perform basic hardware/software troubleshooting.",
+                title: "Amherst Senior Center - Volunteer",
+                date: "Oct 2022 to Jan 2023",
+                description: "Worked alongside the Amherst Senior Center's volunteer coordinator to create technical guides geared towards staff and members of the aforementioned senior center.",
             },
-            {
-                title: "Technical Support Specialist / Commonwealth of Massachusetts, Information Technology Division",
-                date: "Summer 20xx",
-                description: "Supported and hosted state-wide agency systems and messaging services on platforms including HP Unix, Linux, MVS, and Windows Active Exchange. Assisted with server builds and maintenance. Reviewed, installed, set up, and assisted with active synching of BlackBerry mobile devices to Windows Email Exchange server.",
-            },
-            {
-                title: "Asperger’s Association of New England",
-                date: "Jun. 20xx - Aug. 20xx",
-                description: "Volunteered one day a week with data entry and basic financial accounting projects",
-            },
-            {
-                title: "Lifeguard",
-                date: "Summers 20xx – 20xx",
-                description: "YMCA, Lowell, MA",
-            },
+          
+           
         ],
     },
 
-    posts: {
-        name: "Sara Friends",
-        avatar: "https://mui.com/static/images/avatar/1.jpg",
-        background_image: "https://source.unsplash.com/random?wallpapers",
-        post_data: [
-            {
-                id: 1,
-                timestamp: "2 hours ago",
-                description: "Enjoying a sunny day at the beach!",
-                image: "https://source.unsplash.com/random?wallpapers"
-            },
-            {
-                id: 2,
-                timestamp: "5 hours ago",
-                description: "Just finished reading this amazing book.",
-                type: "note",
-            },
-            {
-                id: 3,
-                timestamp: "2 hours ago",
-                description: "Enjoying a sunny day at the beach!",
-                image: "https://source.unsplash.com/random?wallpapers"
-            },
-            {
-                id: 4,
-                timestamp: "5 hours ago",
-                description: "Just finished reading this amazing book.",
-                type: "note",
-            },
-            {
-                id: 5,
-                author: "John Doe",
-                avatar: "/path-to-avatar1.jpg",
-                timestamp: "2 hours ago",
-                description: "Enjoying a sunny day at the beach!",
-                image: "https://source.unsplash.com/random?wallpapers"
-            },
-        ],
-    }
+    
 };
 
 // Exporting the data object so that the App.js file can import it.
